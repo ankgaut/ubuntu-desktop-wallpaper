ca847d9c820937652455cd0f9c5892982b51ed2f
Ankur Gautam <ankurgautam379@gmail.com>
2018-08-06 13:01:47 +0530

    getpics.sh working

--- getpics.sh
+++ getpics.sh
@@ -4,13 +4,16 @@ rm -r apod/
 mkdir apod/
 #rm ~/apod/*.jpg ~/apod/*.html ~/apod/*.txt
 user=$(whoami)
-cd ~/apod
+
+cd apod/
 wget http://apod.nasa.gov/apod/astropix.html
 a='http://apod.nasa.gov/apod/'
-#grep 'href="i' astropix.html > temp.txt
-#b=$(awk -F "\"" '{print $2}' temp.txt) 
-#c=$a$b
-#wget $c
-#imname=$(ls *.jpg)
-#/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
-#cd ~/Desktop
+grep '< *IMG' astropix.html > temp.txt
+b=$(awk -F '"' '{print $2}' temp.txt) 
+c=$a$b
+wget $b
+imname=$(ls *.gif)
+echo $imname
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
+rm temp.txt
+cd ~/Desktop
